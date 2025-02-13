fc38a6bc3a098aa2122363b8634410c4bd3ba11f
hectormorenosoftware <hectormorenosoftware@gmail.com>
2025-02-13 15:37:02 -0600

    Q and A page looks more futuristic after css changes

--- src/components/QandA.js
+++ src/components/QandA.js
@@ -29,8 +29,11 @@ class QandA extends React.PureComponent {
           </button>
           <h1 style={{ fontWeight: "350" }}>Q and A</h1>
           <p className="ParagraphIDQandA">
-            How can I deploy a new website into the internet if I'm not a
-            Software Engineer and I don't have experience in tech? <br />
+            {" "}
+            Question: How can I deploy a new website into the internet if I'm
+            not a Software Engineer and I don't have experience in tech? <br />
+          </p>
+          <p className="ParagraphIDQandA">
             My answer: In &nbsp;
             <a href="https://www.netlify.com/" target="_blank">
               www.netlify.com
@@ -79,23 +82,33 @@ class QandA extends React.PureComponent {
           </p>
 
           <p className="ParagraphIDQandA">
-            Why do I choose React JS over Vue JS, Angular 18, or Angular 19?{" "}
-            <br />
+            {" "}
+            Question: Why do I choose React JS over Vue JS, Angular 18, or
+            Angular 19?{" "}
+          </p>
+
+          <p className="ParagraphIDQandA">
             My answer: I chose React JS because Facebook created React JS and
             uses React JS in Facebook and Instagram. Which means that this
             library can be trusted at scale with large amounts of API requests
             and data.
           </p>
           <p className="ParagraphIDQandA">
-            Why do I choose Node JS over Java Spring Boot and what are the
-            advantages of choosing Node JS as a Back-End? <br />
+            Question: Why do I choose Node JS over Java Spring Boot and what are
+            the advantages of choosing Node JS as a Back-End?{" "}
+          </p>
+          <p className="ParagraphIDQandA">
             My answer: I choose Node JS because it uses libuv which is a library
             in C to handle all asynchronous behavior, this means that all
             asynchronous behavior will be very fast compared to using Java.
             Apple Inc. uses Node JS to make money that is important to know.
           </p>
           <p className="ParagraphIDQandA">
-            What is your recommendation for cloud based database systems? <br />
+            {" "}
+            Question: What is your recommendation for cloud based database
+            systems? <br />
+          </p>
+          <p className="ParagraphIDQandA">
             My answer: If you want control over your data I recommend you to use
             ngrok, it will allow you to write and read data from your own
             machine in your own building if you use Node JS. It will also allow
@@ -103,24 +116,26 @@ class QandA extends React.PureComponent {
             like, remember that your machine and your router will be used to
             serve the data including your website or you could use Mongo DB.
           </p>
-
           <p className="ParagraphIDQandA">
-            Do you have your college transcript with you? <br />
+            {" "}
+            Question: Do you have your college transcript with you?
+          </p>
+          <p className="ParagraphIDQandA">
             My answer: I do, I graduated from the University of Texas at
             Arlington with a B.S. in Computer Science in 2015 my minor is also
             Computer Science I do have my transcript with me wherever I go.
           </p>
+          <p className="ParagraphIDQandA">Question: Do you have a github? </p>
           <p className="ParagraphIDQandA">
-            Do you have a github? <br />
             My answer: github.com/hectormorenosoftware <br />
             Every single repository in my github has a url in the README.md
             section to show you the website live in the internet.
           </p>
-
           <p className="ParagraphIDQandA">
-            How do you test potential candidates for UI Development or Back-End
-            Development?
-            <br />
+            Question: How do you test potential candidates for UI Development or
+            Back-End Development?
+          </p>
+          <p className="ParagraphIDQandA">
             My answer: For UI Development I test a candidate to check if the
             candidate knows what promises are and if the candidate can
             manipulate data in the UI. For Back-End I test to check if the
@@ -128,10 +143,11 @@ class QandA extends React.PureComponent {
             knows how to write and read into a database. I also check if the
             candidate knows how to test API's before they are in production.
           </p>
-
           <p className="ParagraphIDQandA">
-            My laptop and my desktop computer sometimes gets very slow and
-            sometimes it shuts down, why does this happen? <br />
+            Question: My laptop and my desktop computer sometimes gets very slow
+            and sometimes it shuts down, why does this happen?{" "}
+          </p>
+          <p className="ParagraphIDQandA">
             My answer: It's because you have too many desktop applications open
             and because you have too many Google Chrome, Firefox, Safari,
             Microsoft Edge, or Internet Explorer browser windows or browser tabs
@@ -149,16 +165,18 @@ class QandA extends React.PureComponent {
             Microsoft Power Point.
           </p>
           <p className="ParagraphIDQandA">
-            My iphone and my android phone ran out of memory which makes it
-            impossible for me to take new pictures, I don't want to buy a new
-            phone what can I do about this? Are there any options that are
-            affordable? <br />
+            Question: My iphone and my android phone ran out of memory which
+            makes it impossible for me to take new pictures, I don't want to buy
+            a new phone what can I do about this? Are there any options that are
+            affordable?{" "}
+          </p>
+          <p className="ParagraphIDQandA">
             My answer: Yes there is an affordable option, you can use iCloud for
             iOS or Google Cloud Photos and save all of your photos and videos
-            there. After that you can delete every picture from you phone since
+            there. After that you can delete every picture from your phone since
             it's saved already in iCloud or Google Cloud Photos. iCloud and
-            Google Cloud is very affordable is like having a USB in the internet
-            of at least 100GB for like $5 per month.
+            Google Cloud is very affordable is like having a USB Hard Drive in
+            the internet of at least 100GB for like $5 per month.
           </p>
         </div>
       </div>
