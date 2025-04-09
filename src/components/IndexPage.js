0095342935a75e3b5f11ab282918d7c1048a7f67
hectormorenosoftware <hectormorenosoftware@gmail.com>
2025-04-08 23:32:20 -0500

    V12SV

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -33,13 +33,6 @@ class IndexPage extends React.PureComponent {
         <Header />
         <div className="Hero" style={{ height: "100vh", overflowY: "hidden" }}>
           <div className="HeroGroup" style={{ height: "100vh" }}>
-            <video
-              autoPlay={true}
-              loop={true}
-              muted={true}
-              playsInline={true}
-              src="https://websitebuckethectormorenogomez.s3.us-east-2.amazonaws.com/dronerecording.mp4"
-            />
             <h1>Welcome!</h1>
             <p style={{ animationDelay: "4s" }}>
               Please tap or click on the navbar to see more
